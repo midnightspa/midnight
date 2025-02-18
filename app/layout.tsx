a9b233eb2588feb9fa9864d98b68b12cba26158b
mounirActualMarketing <your-email@example.com>
2025-02-18 18:21:40 +0100

    feat: add Google Tag Manager

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from 'next';
 import { Poppins } from 'next/font/google';
 import MainLayout from './components/MainLayout';
 import { Toaster } from 'sonner';
+import Script from 'next/script';
 
 const poppins = Poppins({
   subsets: ['latin'],
@@ -22,7 +23,26 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-NG3W9KNS');
+          `}
+        </Script>
+      </head>
       <body className={poppins.className} suppressHydrationWarning>
+        <noscript>
+          <iframe 
+            src="https://www.googletagmanager.com/ns.html?id=GTM-NG3W9KNS"
+            height="0" 
+            width="0" 
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
         <MainLayout>{children}</MainLayout>
         <Toaster />
       </body>
