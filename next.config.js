79fd9a2368c9ef6ac58fad772fa60f46edf7a893
mounirActualMarketing <your-email@example.com>
2025-02-21 23:15:33 +0100

    fix: update Next.js image configuration to handle both HTTP and HTTPS

--- next.config.js
+++ next.config.js
@@ -12,6 +12,10 @@ const nextConfig = {
       },
       {
         protocol: 'http',
+        hostname: 'themidnightspa.com',
+      },
+      {
+        protocol: 'http',
         hostname: 'localhost',
       },
       {
